5a56c4000a9f8a229b3fcbfdca64363f1e6f2824
Thanh  Vu <vuvanthanh.haui.ktpm1@gmail.com>
2019-06-11 08:15:11 +0700

    Initial Commit

---
+++ DemoCI/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DemoCI
+//
+//  Created by Thanh Vu on 6/11/19.
+//  Copyright © 2019 Ominext. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DemoCI/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DemoCI
+//
+//  Created by Thanh Vu on 6/11/19.
+//  Copyright © 2019 Ominext. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
